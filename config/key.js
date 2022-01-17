0839401cdf6201b3fb3202b877eff9d5e135ec56
Tristen Hogue <smileyhogue@users.noreply.github.com>
2022-01-16 23:57:14 -0700

    Update key.js

--- config/key.js
+++ config/key.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb+srv://admin:Th8971341@cluster0.amj3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MongoURI: "mongodb+srv://admin:password@cluster0.amj3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
